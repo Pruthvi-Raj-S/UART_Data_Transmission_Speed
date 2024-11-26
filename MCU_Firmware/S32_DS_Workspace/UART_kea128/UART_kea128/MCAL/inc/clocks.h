559f1d90de854a2a184e799a2e87f5b9df234a7a
Pruthvi_Raj_S <pruthviraj.s@exicom.in>
2024-11-26 15:45:25 +0530

    V0.0
    -> Updated MCAL config and other main includes and sources

---
+++ MCU_Firmware/S32_DS_Workspace/UART_kea128/UART_kea128/MCAL/inc/clocks.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+* @file: clocks.h
+*
+* @brief: header file for Internal Clock Source utilities.
+* 			provide APIs for accessing internal clock source
+*
+*
+* @Author: Pruthvi Raj S
+*
+* @version: 0.1
+* 			- Initial version
+*
+* @Created on: 26-Nov-2024:
+******************************************************************************/
+
+
+#ifndef CLOCKS_H_
+#define CLOCKS_H_
+
+void init_clks_FEI_48MHz(void);
+void init_clks_FEE_40MHz(void);
+
+#endif /* CLOCKS_H_ */
+
+/******************************************************************************
+*							EOF
+******************************************************************************/
